f397a2a37a111d17eb6e9336af9524eb934aa8cd
Jaylen Yu <114912854+jaylenyu@users.noreply.github.com>
2023-07-15 23:04:18 +0900

    chapter4

---
+++ section2/src/chapter4.ts
@@ -0,0 +1,47 @@
+// type alias // 타입 별칭
+
+import { type } from "os";
+
+type User = {
+    id: number;
+    name: string;
+    nickname: string;
+    birth: string;
+    bio: string;
+    location: string;
+}
+
+let user1: User = {
+    id: 1,
+    name: "jay",
+    nickname: "len",
+    birth: "1020",
+    bio: "hello",
+    location: "London"
+}
+
+let user2: User = {
+    id: 1,
+    name: "lee",
+    nickname: "kangin",
+    birth: "1030",
+    bio: "hello",
+    location: "PSG"
+}
+
+console.log(user1);
+console.log(user2);
+
+
+// index signature
+
+type contryCodesType = {
+    [key : string] : string
+}
+
+let contryCodes:contryCodesType = {
+    Korea: "ROK",
+    UnitedKingdom: "UK",
+    France: "FRA",
+    Sweden: "SWE",
+}
