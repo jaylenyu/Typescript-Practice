a5393222b8e5e6bdc52f7c1ae20b72d2ab8d310a
Jaylen Yu <114912854+jaylenyu@users.noreply.github.com>
2023-07-15 21:20:24 +0900

    chapter1

---
+++ section2/src/chapter1.ts
@@ -0,0 +1,34 @@
+import { isUndefined } from "util";
+
+// Number
+let a: number = 223;
+let b: number = 0.3323;
+
+a = 1233;
+a = 123333;
+b = -77;
+// a = "assd" error
+
+// String
+let naver: string = "naver";
+let google: string = "google";
+let string: string = `hello`;
+let stirng2: string = `hello${naver}`;
+
+// Boolean
+let bool1: boolean = true;
+let bool2: boolean = false;
+
+// null
+let Null: null = null;
+
+// undefined
+let Undefined: undefined = undefined;
+
+// Literal Type
+let literal: 10 = 10;
+literal = 10;
+// literal = 11; error
+let stringType: "string";
+stringType = "string";
+//stringType = "hello"; error
