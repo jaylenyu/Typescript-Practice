25cd840e01b85b27d7333c5ac3c236d2d10c1135
Jaylen Yu <114912854+jaylenyu@users.noreply.github.com>
2023-07-15 21:50:37 +0900

    chapter2

---
+++ section2/src/chapter2.ts
@@ -0,0 +1,29 @@
+// Array
+let numArr: number[] = [1, 3, 2, 9];
+
+let strArr: string[] = ["string", "arr", "hello"];
+
+let boolArr: Array<boolean> = [true, false, false, true];
+
+// mulArr
+let mulArr: (number | string)[] = [1, "string", 77, "hello"];
+
+// multi-dimensionalArr
+let muldiArr: number[][] = [
+    [3, 2, 55],
+    [1, 8]
+]
+
+// tuple
+let tup1: [number, number] = [2, 4];
+
+let tup2: [number, string, boolean] = [3, "string", false];
+
+let userInfo: [string, number, number][] = [
+    ["이름1", 33, 1033244232],
+    ["이름2", 29, 1042324232],
+    ["이름3", 27, 1077752097],
+    ["이름4", 37, 1023347833],
+    ["이름5", 30, 1009894799],
+    // [33, "이름6", 1034300230] error
+]
