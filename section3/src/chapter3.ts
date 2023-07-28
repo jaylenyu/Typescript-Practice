f8062d4f87045a9fe4ae97fe50c9e8ed117afef2
Jaylen Yu <114912854+jaylenyu@users.noreply.github.com>
2023-07-28 17:27:37 +0900

    chapter2, 3

--- section3/src/chapter3.ts
+++ section3/src/chapter3.ts
@@ -25,6 +25,7 @@ let dog: Dog = {
 animal = dog // animal이 dog의 슈퍼타입
 // dog = animal // dog은 animal의 서브타입이라서 error
 
+
 // 초과 프로퍼티 검사
 
 type Book = {
@@ -48,3 +49,15 @@ let programmingBook: ProgrammingBook = {
     price: 15000,
     skill: "react"
 }
+
+book = programmingBook;
+// programmingBook = book // book은 programmingBook의 슈퍼타입
+
+let Book2: Book = {
+    name: "program",
+    price: 15000,
+    // skill: "react"
+}
+// 변수를 초기화할 때, 초기화하는 값으로 객체리터럴을 사용하면 발동하는 검사
+// 실제 타입에는 정해놓지 않은 초과되는 프로퍼티를 작성하면 안되도록 막음
+// 주석처리하거나 삭제하여 객체프로퍼티에 지정된 타입만 쓸 수 있도록 해야함 
