76065eeb7a63464f28f6738e553a4d59086fa32f
Jaylen Yu <114912854+jaylenyu@users.noreply.github.com>
2023-07-29 21:20:15 +0900

    chapter0

---
+++ section4/src/chapter0.ts
@@ -0,0 +1,27 @@
+// 함수타입의 정의
+// 어떤 값을 매개변수로 받고 어떤 값을 반환하는가
+
+function func(a: number, b: number) {
+    return a + b
+}
+
+// 선택적 매개변수
+function introduce(name: string, tall: number, age?: number) {
+    console.log(`name : ${name}`);
+    console.log(`tall : ${tall + 10}`);
+    console.log(`age : ${age}`);
+    
+}
+
+introduce('정인', 190, 32)
+
+
+// 
+function getSum(...rest: number[]) {
+    let sum = 0;
+    rest.forEach((it) => sum += it)
+    return sum;
+}
+
+console.log(getSum(1, 2, 3)) // 6
+console.log(getSum(1, 2, 3, 4, 5)) // 15
